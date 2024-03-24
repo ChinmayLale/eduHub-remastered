d4aa028f34f1adc7e21c0bac2b9312e38f8d3ff2
ChinmayLale <chinmay29.lale@gmail.com>
2024-03-24 18:11:50 +0530

    courses added in hero 4 , motha vattay , all good !

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,25 +5,31 @@ import heropic from './assets/heroimg2.png';
 import book from './assets/ml.svg';
 import DataS from './assets/Data_s.svg';
 import herobg3 from './assets/hero3-bg.png';
+
+
+
+
+
+
 function Hero() {
-  const [courses , setCourses] = useState(null);
-  const [showCourse , setShowCourse] = useState(3);
-  useEffect(()=>{
-      fetch("http://localhost:8000/get").then(
-        (res)=>res.json()
-      ).then((data)=>{
-        console.log(data.users);
-        console.log(typeof(data.users));
-        setCourses(data.users)
-        console.log("SetCourses = "+courses);
-      })
-  },[])
-  
+  const [courses, setCourses] = useState(null);
+  const [showCourse, setShowCourse] = useState(3);
+  useEffect(() => {
+    fetch("http://localhost:8000/get").then(
+      (res) => res.json()
+    ).then((data) => {
+      console.log(data.users);
+      console.log(typeof (data.users));
+      setCourses(data.users)
+      console.log("SetCourses = " + courses);
+    })
+  }, [])
+
   const moreCourse = () => {
-    setShowCourse(showCourse+3);
+    setShowCourse(showCourse + 3);
   }
 
-
+  const star = 5;
 
 
 
@@ -55,16 +61,16 @@ function Hero() {
         <h1>Discover <span>10+ Categories</span> of Online Classes</h1>
         <h2>Unlock limitless knowledge !</h2>
         <div className="features" >
-          {courses && courses.slice(0,showCourse).map((course) =>(
+          {courses && courses.slice(0, showCourse).map((course) => (
             // console.log("CourseName : "+course.course);
-            <div className="feature-card"  key={course.id}>
-            <img src={book} alt="" />
-            <h3 style={{textTransform:'capitalize'}}>{course.course}</h3>
-            <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia, aliquid.</h4>
-            <h5>2+ Courses</h5>
-          </div>
-            
-            
+            <div className="feature-card" key={course.id}>
+              <img src={book} alt="" />
+              <h3 style={{ textTransform: 'capitalize' }}>{course.course}</h3>
+              <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia, aliquid.</h4>
+              <h5>2+ Courses</h5>
+            </div>
+
+
           )
           )}
           <div className="feature-card">
@@ -86,7 +92,7 @@ function Hero() {
             <h5>10+ Courses</h5>
           </div>
         </div>
-        <button className='morecourse'onClick={moreCourse}>Discover More</button>
+        <button className='morecourse' onClick={moreCourse}>Discover More</button>
       </div>
       <div className="hero3">
         <div className="left">
@@ -121,6 +127,58 @@ function Hero() {
       <div className="hero4">
         <h3>POPULAR COURSES</h3>
         <h1>Pick A Course To Get Started</h1>
+        <div className="course-card-div">
+          <div className="course-card">
+            <img src="https://images.unsplash.com/photo-1541178735493-479c1a27ed24?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <div className='course-info'>
+              <h3>beginner</h3>
+              <div className='stars'>
+              <i class="ri-star-fill"></i>
+              <i class="ri-star-fill"></i>
+              <i class="ri-star-fill"></i>
+              <i class="ri-star-half-line"></i>
+              <i class="ri-star-line"></i>
+              <h3>(3.5 / 100) ratings</h3>
+              </div>
+              <h1>Build Responsive Real- World Websites with HTML and CSS</h1>
+              <h1><span>Rs 1,000</span></h1>
+            </div>
+          </div>
+
+          <div className="course-card">
+            <img src="https://images.unsplash.com/photo-1541178735493-479c1a27ed24?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <div className='course-info'>
+              <h3>beginner</h3>
+              <div className='stars'>
+              <i class="ri-star-fill"></i>
+              <i class="ri-star-fill"></i>
+              <i class="ri-star-fill"></i>
+              <i class="ri-star-half-line"></i>
+              <i class="ri-star-line"></i>
+              <h3>(3.5 / 100) ratings</h3>
+              </div>
+              <h1>Build Responsive Real- World Websites with HTML and CSS</h1>
+              <h1><span>Rs 1,000</span></h1>
+            </div>
+          </div>
+
+          <div className="course-card">
+            <img src="https://images.unsplash.com/photo-1541178735493-479c1a27ed24?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <div className='course-info'>
+              <h3>beginner</h3>
+              <div className='stars'>
+              <i class="ri-star-fill"></i>
+              <i class="ri-star-fill"></i>
+              <i class="ri-star-fill"></i>
+              <i class="ri-star-half-line"></i>
+              <i class="ri-star-line"></i>
+              <h3>(3.5 / 100) ratings</h3>
+              </div>
+              <h1>Build Responsive Real- World Websites with HTML and CSS</h1>
+              <h1><span>Rs 1,000</span></h1>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
